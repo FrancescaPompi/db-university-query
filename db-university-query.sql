7d2e1dc46634382ccbac6854c7ede4db2210de7b
FrancescaPompi <93258660+FrancescaPompi@users.noreply.github.com>
2021-12-15 16:48:47 +0100

    Altre 4 query

--- db-university-query.sql
+++ db-university-query.sql
@@ -26,3 +26,25 @@ from departments d;
 select count(*) as Teachers_without_phone
 from teachers t
 where phone is null;
+
+-- 7
+select year(enrolment_date), count(*) as Students_per_year
+from students s 
+group by year(enrolment_date);
+
+-- 8
+select exam_id, avg(vote) as Avarage_grades 
+from exam_student es 
+group by exam_id;
+
+-- 9
+select department_id, count(*) as Number_of_degrees_per_Department
+from degrees d 
+group by department_id;
+
+-- 10
+select s.name, s.surname, s.registration_number 
+from degrees d 
+inner join students s 
+on d.id = s.degree_id
+where d.name = 'Corso di Laurea in Economia';
