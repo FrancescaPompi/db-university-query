797c5400cde9a2748a9506a68e14125f6128f1b7
FrancescaPompi <93258660+FrancescaPompi@users.noreply.github.com>
2021-12-15 17:21:07 +0100

    Ultime 3 query

--- db-university-query.sql
+++ db-university-query.sql
@@ -48,3 +48,28 @@ from degrees d
 inner join students s 
 on d.id = s.degree_id
 where d.name = 'Corso di Laurea in Economia';
+
+-- 11
+select d2. name as Department_name, d.name as Degree_name, d.level as Degree_level
+from degrees d 
+inner join departments d2 
+on d2.id = d.department_id
+where d2.name = 'Dipartimento di Neuroscienze' and d.name like '%Magistrale%';
+
+-- 12
+select c.name as Course, t.name as Teacher_name, t.surname as Teacher_surname
+from courses c 
+inner join course_teacher ct 
+on c.id = ct.course_id 
+inner join teachers t 
+on t.id = ct.teacher_id 
+where t.name = 'Fulvio' and t.surname = 'Amato';
+
+-- 13
+select s.surname as Student_surname, s.name as Student_name, d.name as Degree_name, d2.name as Department_name
+from students s 
+inner join degrees d 
+on d.id = s.degree_id 
+inner join departments d2 
+on d2.id = d.department_id
+order by s.surname, s.name;
