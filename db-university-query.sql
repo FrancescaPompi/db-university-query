9255c5392fec91e270220cc0f90f9faee0cba653
FrancescaPompi <93258660+FrancescaPompi@users.noreply.github.com>
2021-12-15 16:07:53 +0100

    Altre 3 query

--- db-university-query.sql
+++ db-university-query.sql
@@ -12,3 +12,17 @@ where cfu > 10;
 select *
 from students s 
 where timestampdiff(year, date_of_birth, curdate()) > 30;
+
+-- 4
+select *
+from degrees d 
+where level = 'magistrale';
+
+-- 5
+select count(*) as Number_of_Departments
+from departments d;
+
+-- 6
+select count(*) as Teachers_without_phone
+from teachers t
+where phone is null;
